9b117999b71703abc9f6eae3f899e6c40d024bdd
Hiroaki ENDOH <info@hiroakit.com>
2013-09-21 20:33:57 +0900

    cmake-modeの設定を追加。

---
+++ emacs/inits/60-cmake.el
@@ -0,0 +1,8 @@
+;;; cmake-mode
+
+(when (locate-library "cmake-mode")
+  (autoload 'cmake-mode' "cmake-mode" "" t)
+)
+
+(add-to-list 'auto-mode-alist '("CMakeLists\\.txt\\'" . cmake-mode))
+(add-to-list 'auto-mode-alist '("\\.cmake\\'" . cmake-mode))
