0502b2c17de424f8c6e8498cf293a16698283a17
Hiroaki ENDOH <info@hiroakit.com>
2013-10-22 02:08:18 +0900

    package.elを追加
    以下をpackage.elから取得するように変更
    - yasnippet
    - popup
    - scala-mode2
    - auto-complete

---
+++ emacs/inits/20-package.el
@@ -0,0 +1,23 @@
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(package-initialize)
+
+(require 'cl)
+
+(defvar installing-package-list
+  '(
+    ;; ここに使っているパッケージを書いていく
+    scala-mode2
+    yasnippet
+    auto-complete
+    foreign-regexp
+    ))
+
+(let ((not-installed (loop for x in installing-package-list
+                            when (not (package-installed-p x))
+                            collect x)))
+  (when not-installed
+    (package-refresh-contents)
+    (dolist (pkg not-installed)
+        (package-install pkg))))
