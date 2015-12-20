f16ced197f843eb77d84d35835c2a9ebab06fc9e
Hiroaki ENDOH <info@hiroakit.com>
2015-12-20 13:56:03 +0900

    Use init-loader on MELPA

--- emacs/init.el
+++ emacs/init.el
@@ -1,4 +1,4 @@
-;;; ロードパスの設定とinit-loaderを読み込むためのファイル
+;;; Emacsの設定
 
 ;;; 定数宣言
 (defconst hp-elpa-dir (concat user-emacs-directory "elpa"))
@@ -7,38 +7,48 @@
 (defconst hp-org-mode-dir (concat (getenv "HOME") "/src/org-mode/org-8.3.2/lisp"))
 (defconst hp-core-conf "~/.emacs.d/init.el")
 (defconst hp-org-conf (concat (concat user-emacs-directory "inits") "/30-org.el"))
+(defvar hp-melpa-url "http://melpa.milkbox.net/packages/")
+(defvar hp-marmalade-url "http://marmalade-repo.org/packages/")
+(defvar hp-use-package-list
+  '(
+    ;; 以下に使用するパッケージを記述する
+    init-loader
+    scala-mode2
+    yasnippet
+    auto-complete
+    foreign-regexp
+    web-mode
+    js2-mode
+    csharp-mode
+    cmake-mode
+    helm
+    ruby-mode
+    ruby-additional
+    ruby-block
+    org-tree-slide
+    )
+)
 
-;;; 各パッケージのパスをload-pathに展開する関数
-(defun hp-expand-load-path (&rest paths)
-  (let (path)
-    (dolist (path paths paths)
-      (let ((default-directory
-              (expand-file-name path)))
-      (add-to-list 'load-path default-directory)
-      (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
-            (normal-top-level-add-subdirs-to-load-path))))))
+;;; パッケージ
+(require 'package)
+(add-to-list 'package-archives (cons "melpa" hp-melpa-url))
+(add-to-list 'package-archives (cons "marmalade" hp-marmalade-url))
+(package-initialize)
 
-;;; load-pathを設定
-(when (file-directory-p (symbol-value 'hp-elpa-dir))
-  (hp-expand-load-path hp-elpa-dir))
-(when (file-directory-p (symbol-value 'hp-site-lisp-dir))
-  (hp-expand-load-path hp-site-lisp-dir))
-(when (file-directory-p (symbol-value 'hp-org-mode-dir))
-  (hp-expand-load-path hp-org-mode-dir))
+;;; 未インストールのパッケージを探す
+(require 'cl)
+(let ((not-installed 
+       (loop for x in hp-use-package-list
+             when (not (package-installed-p x)) collect x)))
+  (when not-installed
+    (package-refresh-contents)
+    (dolist 
+        (pkg not-installed)
+        (package-install pkg))))
 
 ;;; 各パッケージの設定ファイルはinits以下に置く．init-loaderがそれを読み込む
 ;;; ファイル命名規則が存在する (例 : 10-hoge.el)
 (when (require 'init-loader nil t)
+  (setq init-loader-show-log-after-init 'error-only)
   (when (file-directory-p (symbol-value 'hp-inits-dir))
     (init-loader-load hp-inits-dir)))
-
-(defun hp-show-core-conf ()
-  "init.elを開きます"
-  (interactive)
-  (switch-to-buffer (find-file-noselect hp-core-conf)))
-
-(defun hp-show-org-conf ()
-  "org-modeの設定ファイルを開きます"
-  (interactive)
-  (switch-to-buffer (find-file-noselect hp-org-conf)))
-
