da410f9a443661183d8120fd25be327c0429fb0b
Hiroaki ENDOH <hiroakiendoh@gmail.com>
2016-05-02 08:07:09 +0900

    Add helm-swoop settings

--- emacs/inits/21-helm.el
+++ emacs/inits/21-helm.el
@@ -19,9 +19,9 @@
 
 (when (require 'helm-config nil t)
   (helm-mode 1)
-
-  ;; キーバインディング (02-global-keybindigns.elにもキーバインディングを記述している)
   (define-key helm-map (kbd "C-h") 'delete-backward-char)
   (define-key helm-find-files-map (kbd "C-h") 'delete-backward-char)
   (define-key helm-find-files-map (kbd "TAB") 'helm-execute-persistent-action)
   (define-key helm-read-file-map (kbd "TAB") 'helm-execute-persistent-action))
+
+(when (require 'helm-swoop nil t))
