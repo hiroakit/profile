c260bf4e6078f2f0bd11bdcafd78919759c150cb
Hiroaki ENDOH <hiroakiendoh@gmail.com>
2017-03-11 12:55:15 +0900

    Change emacs background color

--- resources/emacs/init.el
+++ resources/emacs/init.el
@@ -770,7 +770,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#CBB16F" :foreground "gray0" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "nil" :family "MigMix 2M"))))
+ '(default ((t (:inherit nil :stipple nil :background nil :foreground "gray0" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "nil" :family "MigMix 2M"))))
  '(border ((t (:background "light green"))))
  '(cursor ((((class color) (background dark)) (:background "#00AA00")) (((class color) (background light)) (:background "#999999")) (t nil)))
  '(fringe ((t nil)))
