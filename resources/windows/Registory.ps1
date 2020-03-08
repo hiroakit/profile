4c498ff66e7b90f24f5e1b46366dd582f1f76dab
Hiroaki ENDOH <hiroakiendoh@gmail.com>
2020-03-08 22:45:57 +0900

    Add Windows registory updating script

---
+++ resources/windows/Registory.ps1
@@ -0,0 +1,46 @@
+# ItemPropertyが存在する場合にtrue、それ以外にfalseを返す
+function HasItemProperty( $Path, $Name ){
+    # パスがレジストリに存在するか確認する
+    if(!(Test-Path $Path)){
+        Write-Host ("$Path not found. Please check path.")
+        return $FALSE
+    }
+
+    # ItemPropertyの存在を確認する
+    $item = Get-ItemProperty $Path -Name $Name -ErrorAction SilentlyContinue
+    if($null -eq $item){
+        return $FALSE
+    }
+    return $TRUE
+}
+
+# 開発者モードを有効にする
+#
+# HKLM => HKLM HKEY_LOCAL_MACHINE
+# HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\AppModelUnlock
+#
+# AllowAllTrustedApps:1, AllowDevelopmentWithoutDevLicense:1 で開発者モードが有効になる
+function TurnOnDeveloperMode () {
+  # レジストリのパス
+  $DeveloperModePath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\AppModelUnlock"
+
+  # AllowAllTrustedApps
+  if (HasItemProperty $DeveloperModePath AllowAllTrustedApps) {
+    Set-ItemProperty -Path $DeveloperModePath -Name AllowAllTrustedApps -Value 1 
+    Write-Host ("Update AllowAllTrustedApps value.")
+  } else {
+    New-ItemProperty -Path $DeveloperModePath -Name AllowAllTrustedApps -PropertyType DWord -Value 1
+    Write-Host ("Add AllowAllTrustedApps.")
+  }
+
+  # AllowDevelopmentWithoutDevLicense
+  if (HasItemProperty $DeveloperModePath AllowDevelopmentWithoutDevLicense) {
+    Set-ItemProperty -Path $DeveloperModePath -Name AllowDevelopmentWithoutDevLicense -Value 1 
+    Write-Host ("Update AllowDevelopmentWithoutDevLicense value.")
+  } else {
+    New-ItemProperty -Path $DeveloperModePath -Name AllowDevelopmentWithoutDevLicense -PropertyType DWord -Value 1
+    Write-Host ("Add AllowDevelopmentWithoutDevLicense.")
+  }
+}
+
+TurnOnDeveloperMode
