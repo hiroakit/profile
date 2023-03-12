ba81698b958dd7e79e30971951918eeb28b96496
Hiroaki ENDOH <hiroakiendoh@gmail.com>
2023-03-12 11:59:41 +0900

    Hide menu & tool bar

--- resources/emacs/.emacs.d/early-init.el
+++ resources/emacs/.emacs.d/early-init.el
@@ -1,3 +1,6 @@
+(push '(menu-bar-lines . 0) default-frame-alist)
+(push '(tool-bar-lines . 0) default-frame-alist)
+
 ;; ---------------------------------------------
 ;; テーマ
 ;; init.elで設定すると画面が一瞬点滅するから嫌だ
