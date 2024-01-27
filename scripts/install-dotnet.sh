f4565bbb975da0090d9bc4c9be7845e851b066fd
Hiroaki ENDOH <hiroakiendoh@gmail.com>
2024-01-27 14:49:27 +0900

    Update .NET SDK 6.0.102 to 6.0.126
    See https://github.com/hiroakit/profile/issues/22

--- scripts/install-dotnet.sh
+++ scripts/install-dotnet.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-readonly local DOTNET_PKG_URL="https://download.visualstudio.microsoft.com/download/pr/cc2a94b1-7f3c-44f8-a842-f288a0cff04e/32446a93655522a1f933d4afb5e15836/dotnet-sdk-6.0.102-osx-arm64.pkg"
+readonly local DOTNET_PKG_URL="https://download.visualstudio.microsoft.com/download/pr/1bfd8adc-359a-4fb0-b2d1-0cad866b4ea3/04cb083ce577a7ba60b8c46b6d25a72a/dotnet-sdk-6.0.126-osx-arm64.pkg"
 readonly local DOTNET_PKG=`echo ${DOTNET_PKG_URL} | awk -F/ '{print $(NF-0)}'`
 readonly local WORK_DIR=/tmp/macos-setup
 readonly local WORK_DIR_DOTNET_PKG=/tmp/macos-setup/${DOTNET_PKG}
